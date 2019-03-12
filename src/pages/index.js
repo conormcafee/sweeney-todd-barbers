94baf4e30c7c7f3efe16d99ae33f18a26fc0cc66
Conor McAfee <hello@conormcafee.co.uk>
2019-03-12 18:32:11 +0000

    Bare bones Gatsby Install

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`sweeney todd barbers`, `kilkenny`, `ireland`]} />
+  </Layout>
+)
+
+export default IndexPage
