faf09d5ab9efca741111cfdc139bbb81250d2e90
Conor McAfee <hello@conormcafee.co.uk>
2019-03-20 22:43:12 +0000

    Mapbox integrated

--- src/components/OpeningHours/index.js
+++ src/components/OpeningHours/index.js
@@ -2,42 +2,80 @@ import React from "react"
 import {Flex, Box} from "@rebass/grid"
 import styled from "styled-components"
 import {COLOR} from "../../globals"
+import ReactMapboxGl, { Layer, Feature} from "react-mapbox-gl"
 
-const OpeningHours = () => (
-    <Flex flexWrap="wrap" justifyContent="center" pt={5} pb={6}>
-        <Flex width={1} alignItems="center" justifyContent="center" css={{ maxWidth: '900px'}}>
-            <Box width={1} as="article" px={[3, 4]}>
-                <h2>Opening Hours</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus fringilla suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut at felis tortor..</p>
-            </Box>
-        </Flex>
-        
-        <Hours as="ul" p={[3, 4]} width={1} css={{ maxWidth: '1200px'}} mx="auto" mt={5}>
-            {OPENING_HOURS.map((item, index) => (
-                <Day as="li" width={1/7} px={[3, 4]} key={index}>
-                    <HoursText>{item.title}</HoursText>
-                    {item.open !== null && item.close !== null 
-                    ?
-                        <React.Fragment>
-                            <HoursText sep={true}>Open</HoursText>
-                            <HoursText>{item.open}</HoursText>
-                            <HoursText sep={true}>Close</HoursText>
-                            <HoursText>{item.close}</HoursText>
-                        </React.Fragment>
-                    :
-                        <HoursText sep={true}>Closed</HoursText>
-                    }
-                </Day>
-            ))}
-        </Hours>
-    </Flex>
-)
+const Map = ReactMapboxGl({
+    accessToken: "pk.eyJ1IjoiY29ub3JtY2FmZWUiLCJhIjoiY2p0aHJxZDE5MTBqaDN5cnJvOTkyOWc2aSJ9.dz0lrgHAY72fhJ-yirEHmA"
+});
+
+class OpeningHours extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Flex flexWrap="wrap" justifyContent="center" pt={5}>
+                    <Flex width={1} alignItems="center" justifyContent="center" css={{ maxWidth: '900px'}}>
+                        <Box width={1} as="article" px={[3, 4]}>
+                            <h2>Opening Hours</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus fringilla suscipit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut at felis tortor..</p>
+                        </Box>
+                    </Flex>
+                    
+                    <Hours as="ul" p={[3, 4]} width={1} css={{ maxWidth: '1200px'}} mx="auto" mt={5}>
+                        {OPENING_HOURS.map((item, index) => (
+                            <Day as="li" width={1/7} px={[3, 4]} key={index}>
+                                <HoursText>{item.title}</HoursText>
+                                {item.open !== null && item.close !== null 
+                                ?
+                                    <React.Fragment>
+                                        <HoursText sep={true}>Open</HoursText>
+                                        <HoursText>{item.open}</HoursText>
+                                        <HoursText sep={true}>Close</HoursText>
+                                        <HoursText>{item.close}</HoursText>
+                                    </React.Fragment>
+                                :
+                                    <HoursText sep={true}>Closed</HoursText>
+                                }
+                            </Day>
+                        ))}
+                    </Hours>
+                </Flex>
+
+                <MapBox>
+                    <Map
+                        // eslint-disable-next-line
+                        style={'mapbox://styles/conormcafee/cjths1n6g7vu21fpexmcbev7i'}
+                        minZoom={20}
+                        center={[-7.2494442, 52.6524416]}
+                        zoom={[15]}
+                        scrollZoom={[false]}
+                        interactive={[false]}
+                        dragPan={[false]}
+                        containerStyle={{
+                            height: "75vh",
+                            width: "100vw"
+                        }}>
+                            <Layer
+                                scrollZoom={false}
+                                type="symbol"
+                                id="marker"
+                                layout={{ "icon-image": "marker-15" }}>
+                            <Feature />
+                            </Layer>
+                    </Map>
+                </MapBox>
+            </React.Fragment>
+        )
+    }
+}
+
 
 export default OpeningHours
 
 const Hours = styled(Flex)`
-    background: ${COLOR.PRIMARY.DARK};
+    background: ${COLOR.BRAND.BASE};
     border-radius: 4px;
+    position: relative;
+    z-index: 9999;
 `
 
 const Day = styled(Flex)`
@@ -55,6 +93,10 @@ const HoursText = styled.span`
 	margin-bottom: 0px;
 	font-size: ${props => props.sep ? `12px` : `inherit`};
 	color: ${props => props.sep ? `${COLOR.PRIMARY.MID}` : `#ffffff`};
+`
+
+const MapBox = styled.div`
+    margin-top: -100px
 `
 
 const OPENING_HOURS = [
