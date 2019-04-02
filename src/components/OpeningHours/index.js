7d5d5edf31cad6e8afc193a2967ece49bb1c10fc
Conor McAfee <hello@conormcafee.co.uk>
2019-04-02 19:43:24 +0100

    Opening hours now hooked up to cms

--- src/components/OpeningHours/index.js
+++ src/components/OpeningHours/index.js
@@ -1,75 +1,90 @@
 import React from "react"
+import {StaticQuery, graphql} from "gatsby"
 import {Flex, Box} from "@rebass/grid"
 import styled from "styled-components"
 import {COLOR} from "../../globals"
 import Map from "../Map"
 import Button from "../Button"
 
-class OpeningHours extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Flex id="opening" flexWrap="wrap" justifyContent="center" pt={5}>
-                    <Flex width={1} alignItems="center" justifyContent="center" css={{ maxWidth: '900px'}}>
-                        <Box width={1} as="article" px={[3, 4]}>
-                            <h2>Opening Hours</h2>
-                            <p>We accept all walk-ins but we advise clients to call in advance to secure their appointment. Hot towel shaving is only available Wednesday - Saturday. We're open every day excluding Sundays and Bank Holidays.</p>
-                            <Button />
-                        </Box>
-                    </Flex>
-                    
-                    <Hours 
-                        as="ul" 
-                        flexWrap={['wrap', 'wrap', 'nowrap']}
-                        p={[3, 4]} 
-                        width={1} 
-                        mx="auto" 
-                        mt={[3, 5]}
-                        css={{ maxWidth: '1200px'}}>
-                        
-                        {OPENING_HOURS.map((item, index) => (
-                            <Day 
-                                as="li"
-                                flexDirection={['row', 'row', 'column']}
-                                justifyContent={['space-between', 'space-between']}
-                                alignItems={['center', 'center']}
-                                width={[1, 1, 1/7]} 
-                                pb={[3, 3, 0]}
-                                px={[3, 4]} 
-                                mb={[3, 3, 0]}
-                                key={index}>
-                                
-                                <HoursText header>{item.title}</HoursText>
-                                {item.open !== null && item.close !== null 
-                                ?
-                                    <Flex flex="1 1 auto" justifyContent="flex-end">
-                                        <Box>
-                                            <HoursText sep={true}>Open</HoursText>
-                                            <HoursText>{item.open}</HoursText>
-                                        </Box>
-                                        <Box ml={4}>
-                                            <HoursText sep={true}>Close</HoursText>
-                                            <HoursText>{item.close}</HoursText>
-                                        </Box>
-                                    </Flex>
-                                :
-                                    <HoursText sep={true}>Closed</HoursText>
-                                }
-                                
-                            </Day>
-                        ))}
+export default () => <StaticQuery query={openingHoursQuery} render={data => <OpeningHours data={data} />} /> 
 
-                    </Hours>
+const OpeningHours = (props) => {
+    const times = props.data.file.childMarkdownRemark.frontmatter.times
+    return (
+        <React.Fragment>
+            <Flex id="opening" flexWrap="wrap" justifyContent="center" pt={5}>
+                <Flex width={1} alignItems="center" justifyContent="center" css={{ maxWidth: '900px'}}>
+                    <Box width={1} as="article" px={[3, 4]}>
+                        <h2>Opening Hours</h2>
+                        <p>We accept all walk-ins but we advise clients to call in advance to secure their appointment. Hot towel shaving is only available Wednesday - Saturday. We're open every day excluding Sundays and Bank Holidays.</p>
+                        <Button />
+                    </Box>
                 </Flex>
+                
+                <Hours 
+                    as="ul" 
+                    flexWrap={['wrap', 'wrap', 'nowrap']}
+                    p={[3, 4]} 
+                    width={1} 
+                    mx="auto" 
+                    mt={[3, 5]}
+                    css={{ maxWidth: '1200px'}}>
+                    
+                    {times.map((item, index) => (
+                        <Day 
+                            as="li"
+                            flexDirection={['row', 'row', 'column']}
+                            justifyContent={['space-between', 'space-between']}
+                            alignItems={['center', 'center']}
+                            width={[1, 1, 1/7]} 
+                            pb={[3, 3, 0]}
+                            px={[3, 4]} 
+                            mb={[3, 3, 0]}
+                            key={index}>
+                            
+                            <HoursText header>{item.day}</HoursText>
+                            {item.open !== null && item.close !== null 
+                            ?
+                                <Flex flex="1 1 auto" justifyContent="flex-end">
+                                    <Box>
+                                        <HoursText sep={true}>Open</HoursText>
+                                        <HoursText>{item.open}</HoursText>
+                                    </Box>
+                                    <Box ml={4}>
+                                        <HoursText sep={true}>Close</HoursText>
+                                        <HoursText>{item.closed}</HoursText>
+                                    </Box>
+                                </Flex>
+                            :
+                                <HoursText sep={true}>Closed</HoursText>
+                            }
+                            
+                        </Day>
+                    ))}
 
-                <Map />
-            </React.Fragment>
-        )
-    }
+                </Hours>
+            </Flex>
+
+            <Map />
+        </React.Fragment>
+    )
 }
 
-
-export default OpeningHours
+const openingHoursQuery = graphql`
+    query {
+        file(name: {eq: "openingHours"}) {
+            childMarkdownRemark {
+                frontmatter {
+                    times {
+                        day
+                        open
+                        closed
+                    }
+                }
+            }
+        }
+    }
+`
 
 const Hours = styled(Flex)`
     background: ${COLOR.BRAND.BASE};
@@ -100,41 +115,3 @@ const HoursText = styled.span`
 	font-size: ${props => props.sep ? `12px` : `inherit`};
 	color: ${props => props.sep ? `${COLOR.PRIMARY.MID}` : `#ffffff`};
 `
-
-const OPENING_HOURS = [
-	{ 
-		title: "Mon",     
-		open: "09:15", 
-		close: "18:00"
-	},
-	{ 
-		title: "Tue",     
-		open: "09:15", 
-		close: "18:00"
-	},
-	{ 
-		title: "Wed",     
-		open: "09:15", 
-		close: "18:00"
-	},
-	{ 
-		title: "Thur",
-		open: "09:00",
-		close: "18:30"
-	},
-	{ 
-		title: "Fri",
-		open: "09:00",
-		close: "18:30"
-	},
-	{ 
-		title: "Sat",
-		open: "08:30",
-		close: "18:00"
-	},
-	{ 
-		title: "Sun",  
-		open: null,
-		close: null
-	}
-]
